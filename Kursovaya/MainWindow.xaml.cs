37d8a9f1e52c47e12d24af328b03f0cc0ab9be52
Biba <Biba@DESKTOP-O1EEU26>
2023-04-06 01:21:33 +0300

    Добавьте файлы проекта.

---
+++ Kursovaya/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursovaya
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public class Row
+    {
+        public string Game { get; set; }
+        public string Date { get; set; }
+        public string FirstWin { get; set; }
+        public string Spare { get; set; }
+        public string SecondWin { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
